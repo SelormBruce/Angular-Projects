b474a834adb311d5582fe9fdf54425e249489a3e
SelormBruce <developerin2020@gmail.com>
2019-01-06 10:33:13 +0000

    google & facebook authentication

--- src/app/Model/auth/login/login.component.ts
+++ src/app/Model/auth/login/login.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostBinding} from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {Router} from '@angular/router';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database"; 
+//import {moveIn} from '../router.animations';
+import * as firebase from 'firebase';
+import { error } from 'util';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  //animations: [moveIn()],
+  //host:{'@moveIn':''}
 })
 export class LoginComponent implements OnInit {
+Error:any;
 
-  constructor() { }
-
+  constructor( private af:AngularFireAuth, private router:Router) 
+  {
+ this.af.auth.onAuthStateChanged(auth =>{
+   if(auth){
+     this.router.navigate(['/members']);
+   }
+ });
+   }
+   LoginWithFaceBook(){
+    this.af.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    .then(suceess=>{this.router.navigate(['/members']);})
+    .catch((error)=>{this.Error= error});
+   }
+   LoginWithGoogle(){
+    this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider)
+    .then(suceess=>{this.router.navigate(['/members']);})
+    .catch((error)=>{this.Error= error});
+   }
   ngOnInit() {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,20 @@ import { HomeComponent } from './Model/info/home/home.component';
 import { AboutComponent } from './Model/info/about/about.component';
 import {AngularFireModule} from 'angularfire2';
 import { environment } from 'src/environments/environment.prod';
+import { EmailComponent } from './Model/auth/email/email.component';
+import { MembersComponent } from './Model/auth/members/members.component';
+
+import { AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    EmailComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +32,10 @@ import { environment } from 'src/environments/environment.prod';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MaterialModule,
-    AngularFireModule.initializeApp(environment.Firebaseconfig)
+    AngularFireModule.initializeApp(environment.Firebaseconfig),
+    AngularFireAuthModule,
+    AngularFireAuth,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
